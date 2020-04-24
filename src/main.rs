67a4b80cf2ce741e9c20e734a2bce81a35ad5caf
Oliver Daff <oliverdaff@users.noreply.github.com>
2020-04-24 22:35:17 +1000

    simple stdin stdout interface to network iocs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate ripioc;
+
+use ripioc::network_ioc::parse_network_iocs;
+use std::io::{self, Read};
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    match io::stdin().read_to_string(&mut input){
+        Ok(_) => {
+            let found_iocs = parse_network_iocs(&input);
+            println!("{:?}", found_iocs);
+        }
+        Err(err) => println!("Error {}", err),
+    }
 }
