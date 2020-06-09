91899769536b1abbc9cf05dda6ff4c367130d6ed
Oliver Daff <oliverdaff@users.noreply.github.com>
2020-06-09 16:03:01 +1000

    cargo format

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,9 @@ extern crate ripioc;
 use ripioc::network_ioc::parse_network_iocs;
 use std::io::{self, Read};
 
-
 fn main() {
     let mut input = String::new();
-    match io::stdin().read_to_string(&mut input){
+    match io::stdin().read_to_string(&mut input) {
         Ok(_) => {
             let found_iocs = parse_network_iocs(&input);
             println!("{:?}", found_iocs);
