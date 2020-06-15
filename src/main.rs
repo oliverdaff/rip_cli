a290128dc7e2f20a6036be25e4365b57372a70c5
Oliver Daff <oliverdaff@users.noreply.github.com>
2020-06-15 14:48:09 +1000

    cargo format

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,43 @@
-extern crate ripioc;
 extern crate clap;
+extern crate ripioc;
 
-
+use clap::{App, Arg, ArgGroup};
 use ripioc::network_ioc::parse_network_iocs;
-use std::io::{self, Read};
 use std::fs;
-use clap::{Arg, ArgGroup, App};
-
-
+use std::io::{self, Read};
 
 // [--input INPUT] default: stdin
 // [--output OUTPUT] default: stdout
 fn main() {
-
     let result = App::new("CLI for rip IOC")
-    .version("0.1")
-    .about("Parser for IOC from input text")
-    .arg(
-        Arg::with_name("stdin")
-            .short('c')
-            .long("stdin")
-            .value_name("STDIN")
-            .about("Read input from stdin")
-            .takes_value(false)
-    )
-    .arg(
-        Arg::with_name("input_file")
-            .short('i')
-            .long("input_file")
-            .value_name("INPUT_FILE")
-            .about("Read input from file")
-            .takes_value(true)
-    )
-    .group(ArgGroup::with_name("input")
-         .args(&["stdin", "input_file"])
-         .required(true)).get_matches();
+        .version("0.1")
+        .about("Parser for IOC from input text")
+        .arg(
+            Arg::with_name("stdin")
+                .short('c')
+                .long("stdin")
+                .value_name("STDIN")
+                .about("Read input from stdin")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("input_file")
+                .short('i')
+                .long("input_file")
+                .value_name("INPUT_FILE")
+                .about("Read input from file")
+                .takes_value(true),
+        )
+        .group(
+            ArgGroup::with_name("input")
+                .args(&["stdin", "input_file"])
+                .required(true),
+        )
+        .get_matches();
 
     let mut input = String::new();
 
-    let read_input_result = if result.is_present("stdin"){
+    let read_input_result = if result.is_present("stdin") {
         io::stdin().read_to_string(&mut input)
     } else {
         let input_file = result.value_of("input_file").unwrap();
@@ -49,10 +48,7 @@ fn main() {
         Ok(_) => {
             let iocs = parse_network_iocs(&input);
             println!("{:?}", iocs);
-        },
+        }
         Err(e) => println!("Error reading input {}", e),
     }
 }
-
-
-
