42f071eb2e551ad67f0385b06ff3b2458d172f4f
Oliver Daff <oliverdaff@users.noreply.github.com>
2020-06-29 14:45:18 +1000

    use in and out files as options

--- src/main.rs
+++ src/main.rs
@@ -4,54 +4,53 @@ extern crate ripioc;
 mod output;
 
 use crate::output::json;
-use clap::{App, Arg, ArgGroup};
+use clap::{App, Arg};
 use ripioc::parse_all_iocs;
 use std::fs;
 use std::io::{self, Read};
 
 // [--input INPUT] default: stdin
 // [--output OUTPUT] default: stdout
-fn main() {
+fn main() -> io::Result<()> {
     let result = App::new("CLI for rip IOC")
         .version("0.1")
         .about("Parser for IOC from input text")
-        .arg(
-            Arg::with_name("stdin")
-                .short("c")
-                .long("stdin")
-                .value_name("STDIN")
-                .help("Read input from stdin")
-                .takes_value(false),
-        )
         .arg(
             Arg::with_name("input_file")
                 .short("i")
                 .long("input_file")
                 .value_name("INPUT_FILE")
                 .help("Read input from file")
-                .takes_value(true),
+                .takes_value(true)
+                .required(false),
         )
-        .group(
-            ArgGroup::with_name("input")
-                .args(&["stdin", "input_file"])
-                .required(true),
+        .arg(
+            Arg::with_name("output_file")
+                .short("o")
+                .long("output_file")
+                .value_name("OUTPUT_FILE")
+                .help("Write output to file")
+                .takes_value(true)
+                .required(false),
         )
         .get_matches();
 
     let mut input = String::new();
 
-    let read_input_result = if result.is_present("stdin") {
-        io::stdin().read_to_string(&mut input)
-    } else {
+    let read_input_result = if result.is_present("input_file") {
         let input_file = result.value_of("input_file").unwrap();
         fs::File::open(&input_file).and_then(|mut f| f.read_to_string(&mut input))
+    } else {
+        io::stdin().read_to_string(&mut input)
     };
 
-    match read_input_result {
-        Ok(_) => {
-            let iocs = parse_all_iocs(&input);
-            println!("{}", json::output_json(&iocs));
+    let iocs = read_input_result.map(|_| parse_all_iocs(&input))?;
+
+    match result.value_of("output_file") {
+        Some(out_file) => fs::write(out_file, json::output_json(&iocs)),
+        None => {
+            println!("{:?}", json::output_json(&iocs));
+            Ok(())
         }
-        Err(e) => println!("Error reading input {}", e),
     }
 }
